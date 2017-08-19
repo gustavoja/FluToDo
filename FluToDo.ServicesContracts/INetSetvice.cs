92bcbcb4486d48a379a7ac78772c07caf33d0ec1
Gustavo Jimenez <gustavoja@gmail.com>
2017-08-19 12:37:51 +0200

    Add NetService objects and TodoItem Entity

---
+++ FluToDo.ServicesContracts/INetSetvice.cs
@@ -0,0 +1,24 @@
+﻿using FluToDo.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluToDo.ServiceContracts
+{
+    public interface INetService
+    {
+        string ServerUrl { get; set; }
+        int Port { get; set; }
+        string ApiRoute { get; set; }
+        string RestServiceUrl { get; }
+
+        Task<List<TodoItem>> GetAll();
+        Task<TodoItem> GetById(string id);
+        Task<TodoItem> Create(TodoItem item);
+        Task<bool> Update(TodoItem item, string id);
+        Task<bool> Delete(TodoItem item);
+
+    }
+}
